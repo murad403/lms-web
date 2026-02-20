51d3bf620ce0b4c6347654c41bae30fecce7407a
murad403 <mdm78774@gmail.com>
2026-02-20 15:10:03 +0600

    feat: enhance ProfileAbout component and update organization profile page layout

--- src/app/[locale]/(organization)/organization/profile/page.tsx
+++ src/app/[locale]/(organization)/organization/profile/page.tsx
@@ -1,9 +1,13 @@
+import ProfileAbout from '@/components/reusable/for-dashboard/ProfileAbout'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      <div className="container mx-auto py-8 space-y-8">
+
+        <ProfileAbout />
+      </div>
     </div>
   )
 }
--- src/components/reusable/for-dashboard/ProfileAbout.tsx
+++ src/components/reusable/for-dashboard/ProfileAbout.tsx
@@ -1,6 +1,8 @@
+import { instructorProfile } from '@/lib/instructor';
 import React from 'react'
 
 const ProfileAbout = () => {
+    const profile = instructorProfile;
     return (
         <div className="bg-white p-6">
             <h2 className="text-sm font-bold text-title mb-3 uppercase tracking-widest">
