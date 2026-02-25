ecf5a7b34e3e3c576896d4d8196d74ddc257cb92
murad403 <mdm78774@gmail.com>
2026-02-25 12:39:10 +0600

    text alignment

--- src/app/[locale]/(app)/home/Courses.tsx
+++ src/app/[locale]/(app)/home/Courses.tsx
@@ -32,7 +32,7 @@ const Courses = () => {
             <div className="container mx-auto">
                 {/* Trending Courses Section */}
                 <div className="mb-12">
-                    <h2 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-header mb-4 md:mb-8 text-center">
+                    <h2 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-header mb-4 md:mb-6">
                         {t("trendingCourses")}
                     </h2>
                     <div className="relative group">
@@ -68,7 +68,7 @@ const Courses = () => {
 
                 {/* Featured Courses Section */}
                 <div className="mb-12">
-                    <h2 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-header mb-4 md:mb-8 text-center">
+                    <h2 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-header mb-4 md:mb-6">
                         {t("featuredCourses")}
                     </h2>
                     <div className="relative group">
@@ -104,7 +104,7 @@ const Courses = () => {
 
                 {/* Most Requested Courses Section */}
                 <div>
-                    <h2 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-header mb-4 md:mb-8 text-center">
+                    <h2 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-header mb-4 md:mb-6">
                         {t("mostRequestedCourses")}
                     </h2>
                     <div className="relative group">
