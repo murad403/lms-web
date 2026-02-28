84a4dc57976f7ed2ea0a146ed9a6ce428046776c
murad403 <mdm78774@gmail.com>
2026-02-28 11:51:35 +0600

    feat: add NotFoundPage component with 404 error handling and image

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import notFoundImage from "@/assets/auth/notfound.png";
+import Image from "next/image";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col md:flex-row min-h-screen justify-center items-center gap-10 max-w-6xl mx-auto px-4 sm:px-0">
+      <div className="space-y-4">
+        <h2 className="font-semibold text-7xl text-[#E9EAF0]">Error 404</h2>
+        <h3 className="font-bold text-navy-blue text-4xl">Oops! page not found</h3>
+        <p className="text-description text-base max-w-sm">{`Something went wrong. It’s look that your requested could not be found. It's look like the link is broken or the page is removed.`}</p>
+        <button className="bg-navy-blue text-white text-base font-medium py-3 px-4">Go Back</button>
+      </div>
+      <div>
+        <Image src={notFoundImage} alt="not found image" width={500} height={500}/>
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
