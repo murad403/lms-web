af1c127dde9483a2c591e1e8a22a70f0eb8747ba
murad403 <mdm78774@gmail.com>
2026-04-28 12:36:35 +0600

    refactor: remove unused PaymentCards and WithdrawSection components from EarningsPage

--- src/app/[locale]/(organization)/organization/earnings/page.tsx
+++ src/app/[locale]/(organization)/organization/earnings/page.tsx
@@ -1,9 +1,7 @@
 "use client"
 import InstructorEarningStats from '@/components/reusable/for-dashboard/InstructorEarningStats'
-import PaymentCards from '@/components/reusable/for-dashboard/PaymentCards'
 import RevenueChart from '@/components/reusable/for-dashboard/RevenueChart'
 import WithdrawHistory from '@/components/reusable/for-dashboard/WithdrawHistory'
-import WithdrawSection from '@/components/reusable/for-dashboard/WithdrawSection'
 import { revenueData } from '@/lib/instructor'
 import { useTranslations } from 'next-intl'
 
@@ -21,14 +19,10 @@ const EarningsPage = () => {
           <RevenueChart pathColor="#23BD33" strokeColor="#23BD33" title={t("statistic")} data={revenueData} />
         </div>
 
-        {/* Cards */}
-        <PaymentCards />
       </div>
 
       {/* Withdraw + Withdrawal History */}
       <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
-        {/* Withdraw Section */}
-        <WithdrawSection />
 
         {/* Withdrawal History */}
         <WithdrawHistory />
